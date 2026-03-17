aadd3220b5c1924d6fab53c2194e2ecb93eddf18
SwapnilSalunke1625 <salunkeswapnil145@gmail.com>
2026-03-17 08:20:07 +0530

    backend is added

---
+++ Server/src/index.js
@@ -0,0 +1,14 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+
+const app=express()
+const PORT=process.env.PORT || 1010
+
+app.get("/", (req,res)=>{
+    res.send("server is created ")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`server is running at port ${PORT}`)
+})
